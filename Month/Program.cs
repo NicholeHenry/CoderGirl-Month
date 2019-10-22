8a81e59a4707f1b9b52e081fdcfdc9df5600502c
Nichole Henry <nichole.henry12@gmail.com>
2019-10-21 23:39:04 -0500

    test passed

--- Month/Program.cs
+++ Month/Program.cs
@@ -6,12 +6,78 @@ namespace Month
     {
         public static void Main()
         {
-            Console.Write("Enter the number of the month: ");
+            string monthName = string.Empty;
 
+            Console.Write("Enter the number of the month: ");
+            string monthNumber = Console.ReadLine();
+
+            if (monthNumber == "1")
+            {
+                monthName = "January";
+            }
+
+             else if (monthNumber == "2")
+            {
+                monthName = "February";
+            }
+
+            else if (monthNumber == "3")
+            {
+                monthName ="March";
+            }
+
+            else if (monthNumber == "4")
+            {
+                monthName = "April";
+            }
+
+            else if (monthNumber == "5")
+            {
+                monthName = "May";
+            }
+
+            else if(monthNumber == "6")
+            {
+                monthName = "June";
+            }
+            else if(monthNumber == "7")
+            {
+                monthName = "July";
+            }
+
+            else if (monthNumber == "8")
+            {
+                monthName = "August";
+            }
+
+            else if (monthNumber == "9")
+            {
+                monthName = "September";
+            }
+
+            else if (monthNumber == "10")
+            {
+                monthName = "October";
+            }
+
+            else if(monthNumber == "11")
+            {
+                monthName = "November";
+            }
+
+            else if(monthNumber == "12")
+            {
+                monthName = "December";
+            }
+
+            else
+            {
+                monthName = "That Month does not exist";
+            }
             // Save the input to a variable.
 
             // Use the if statment to update the monthName variable based on the user's input.
-            string monthName = string.Empty;
+           
 
 
             Console.WriteLine($"The name of the month you entered is {monthName}");
